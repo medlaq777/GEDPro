a288de06aa62c037bca6227e89c532578aaa05dc
medlaq777 <m.laqsoumi@gmail.com>
2025-12-23 11:21:57 +0100

    Make DB connection usin typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabasModule } from './DB/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabasModule,
+  ],
 })
 export class AppModule {}
